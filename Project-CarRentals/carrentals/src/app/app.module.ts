7ce6d66e11cbdee7b7eefba54b99e59ad6283ecc
GollapudiSrikanth <srikanth.sri31@gmail.com>
2022-03-02 10:53:08 -0600

    added top,login,signup,home components

--- Project-CarRentals/carrentals/src/app/app.module.ts
+++ Project-CarRentals/carrentals/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
